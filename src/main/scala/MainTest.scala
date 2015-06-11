4eea7ca127e3c03df74f6884bd053b50f5f8eeb3
vishnu prasad <vishnu667@gmail.com>
2015-06-12 01:13:10 +0530

    Working on slick WIP

---
+++ src/main/scala/MainTest.scala
@@ -0,0 +1,32 @@
+
+import slick.driver.PostgresDriver.api._
+
+
+/**
+ * Class .MainTest
+ * Created by vishnu667 on 12/6/15.
+ * http://queirozf.com/entries/scala-slick-simple-example-on-connecting-to-a-postgresql-database
+ */
+object MainTest {
+
+  class Users(tag: Tag) extends Table[(Int, String)](tag, "users") {
+    def id = column[Int]("id")
+    def username = column[String]("username")
+    def * = (id, username)
+  }
+
+  def main(args: Array[String]) {
+    println("Starting test application")
+
+    val connectionUrl = "jdbc:postgresql://localhost/my-db?user=postgres&password=postgres"
+
+    Database.forURL(connectionUrl, driver = "org.postgresql.Driver") withSession {
+      implicit session =>
+        val users = TableQuery[Users]
+
+        users.schema.create
+
+    }
+    println("Exiting test application")
+  }
+}
